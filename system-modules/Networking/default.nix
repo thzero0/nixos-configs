0e88d17754ff1960a1c120caea3085f6e0cc65f9
thzero0 <thiagozeroa@gmail.com>
2023-08-21 23:32:55 -0300

    modularized networking

---
+++ system-modules/Networking/default.nix
@@ -0,0 +1,15 @@
+{inputs, lib, config, pkgs, ...}:
+
+{
+	networking.hostname = "BlackHole"; # Define my hostname
+	
+	# networking.wireless.enable = true; # Enables wireless support via wpa_supplicant
+
+	# Configures network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+	# Enable networking
+	networking.networkmanager.enable = true;
+
+}
