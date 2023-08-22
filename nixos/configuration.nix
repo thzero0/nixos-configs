30c73a481c8e963d063a34d4711e7ef127e91750
thzero0 <thiagozeroa@gmail.com>
2023-08-22 10:21:40 -0300

    modularized Keymaps

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,43 +15,10 @@
     ../system-modules/grubBoot.nix
     ../system-modules/Audio
     ../system-modules/Networking
+    ../system-modules/Keymap	
 
     ];
 
-
-
-
-
-  # Set your time zone.
-  time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
-
-
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "br";
-    xkbVariant = "";
-  };
-
-
-
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
 
 
 
